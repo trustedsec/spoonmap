52a3ac035a97dc9a7ad9924765a2bc2a7ccb152c
larry.spohn <Larry.Spohn@TrustedSec.com>
2021-01-11 11:18:16 -0500

    Public Release

---
+++ README.md
@@ -0,0 +1,195 @@
+# SpooNMAP
+
+## Dependencies
+This script is simply a wrapper for NMAP and Masscan. Install them from your
+favorite package manager, or install from source.
+
+The script also utilizes Python's magical f-strings, so Python 3.6 or above
+is required.
+
+## Usage
+Again, make sure that you have Python 3.6 or above installed. Simply executing
+the script will prompt you for all of the required scanning options.
+
+```
+# ./spoonmap.py 
+
+________                   _____   _______  _________________ 
+__  ___/______________________  | / /__   |/  /__    |__  __ \
+_____ \___  __ \  __ \  __ \_   |/ /__  /|_/ /__  /| |_  /_/ /
+____/ /__  /_/ / /_/ / /_/ /  /|  / _  /  / / _  ___ |  ____/ 
+/____/ _  .___/\____/\____//_/ |_/  /_/  /_/  /_/  |_/_/      
+       /_/                                                 
+    
+
+Scan Type
+	(1) Small Port Scan
+	(2) Medium Port Scan
+	(3) Large Port Scan
+	(4) Extra Large Port Scan (Small, Medium, and Large)
+	(5) Full Port Scan
+	(6) Custom Port Scan
+
+What type of scan would you like to perform (default: Small Port Scan)? 
+
+Would you like to enumerate service banners for any identified services (default: Yes)? 
+
+Target Scan
+	(1) External
+	(2) Internal
+
+Is this an internal or external scan (default: External)? 
+
+How fast would you like to scan (default: 20000 packets/second)? 
+
+Example Target File
+One CIDR or IP Address per line
+
+	192.168.0.0/24
+	192.168.1.23
+
+Please enter the full path for the file containing target hosts (default: /opt/spoonmap/ranges.txt): 
+
+Scan Type: Small Port Scan
+Target Ports: ['80', '443', '8000', '8080', '8008', '8181', '8443']
+Service Banner: False
+Source Port: 53
+Masscan Max Packet Rate (pps): 2000
+Target File: ranges.txt
+
+Scanning port 80...
+```
+You can also create a configuration file to avoid all of the prompts. Use the
+provided 'config.json.sample' as an example. Just make sure that your file 
+is named 'config.json'
+```
+# cat config.json
+{
+    "__scan_type_choices__" : "Small Port Scan, Medium Port Scan, Large Port Scan, Extra Large Port Scan, Custom Port Scan", 
+    "scan_type" : "Custom Port Scan", 
+    "dest_ports" : ["80","443","8000","8080"],
+    "__banner_scan_choices__" : "True, False", 
+    "banner_scan" : "True", 
+    "__target_scan_choices__" : "External, Internal", 
+    "target_scan" : "Internal",
+    "__max_rate_external_recommedation__" : "Single Port = 20000, Full Port = 10000", 
+    "__max_rate_internal_recommedation__" : "Single Port = 2000, Full Port = 1000", 
+    "max_rate" : "2000",
+    "target_file" : "ranges.txt"
+}
+```
+#### config.json Parameters
+##### scan_type
+This paramater is used to determine what ports to scan.
+* Small Port Scan
+    * 80, 443, 8000, 8080, 8008, 8181, 8443
+* Medium Port Scan
+    * 7001, 1433, 445, 139, 21, 22, 23, 25, \
+    53, 111, 389, 4243, 3389, 3306, 4786, \
+    5900, 5901, 6379, 6970, 9100
+* Large Port Scan
+    * 1090, 1098, 1099, 10999, 11099, 11111, \
+    3300, 4243, 4444, 4445, 45000, 45001, \
+    47001, 47002, 4786, 4848, 50500, 5555, \
+    5556, 6129, 6379, 6970, 7000, \
+    7002, 7003, 7004, 7070, 7071, \
+    8001, 8002, 8003, 8686, 9000, \
+    9001, 9002, 9003, 9012, 9503
+* Extra Large Port Scan
+    * Small, Medium, and Large Ports Combined
+* Full Port Scan
+    * 1-65,535
+* Custom Port Scan
+    * Dealer's Choice
+##### dest_ports
+* Only used if 'Custom Port Scan' is selected.
+##### banner_scan
+This parameter is used to determine whether NMAP will be used
+to grab service banners.
+* True
+* False
+##### target_scan
+This paramater is used to determine what source port to spoof.
+* External Port Scan
+    * source port = 53
+* Internal Port Scan
+    * source port = 88
+##### max_rate
+This parameter is used to determine how fast to scan in masscan.
+If it is not set manually, it is determined from the
+scan_type and target_scan parameters.
+**Note: Selecting a max_rate that is too high can easily create
+a denial-of-service. In my testing, the following rates have been
+found to be safe. YMMV**
+* 'External' and 'Common Port Scan'
+    * max_rate = 20,000 packets/second
+* 'External' and 'Full Port Scan'
+    * max_rate = 10,000 packets/second
+* 'Internal' and 'Common Port Scan'
+    * max_rate = 2,000 packets/second
+* 'Internal' and 'Full Port Scan'
+    * max_rate = 1,000 packets/second
+* Everything else
+    * max_rate = 2,000 packets/second
+    
+## Potential Hacks to Look For  
+
+1090, 1098, 1099, 4444, 11099, 47001, 47002, 10999  
+Java RMI  
+https://www.rapid7.com/db/modules/exploit/multi/misc/java_rmi_server  
+https://medium.com/@afinepl/java-rmi-for-pentesters-structure-recon-and-communication-non-jmx-registries-a10d5c996a79  
+https://medium.com/@afinepl/java-rmi-for-pentesters-part-two-reconnaissance-attack-against-non-jmx-registries-187a6561314d  
+
+7000-7004, 8000-8003, 9000-9003, 9503, 7070, 7071  
+WebLogic  
+https://www.exploit-db.com/search?q=weblogic  
+  
+45000, 45001  
+JDWP  
+https://www.rapid7.com/db/modules/exploit/multi/misc/java_jdwp_debugger  
+https://github.com/IOActive/jdwp-shellifier  
+  
+8686, 9012, 50500  
+JMX  
+https://www.rapid7.com/db/modules/exploit/multi/misc/java_jmx_server  
+  
+4848  
+GlassFish  
+https://www.rapid7.com/db/modules/auxiliary/scanner/http/glassfish_traversal  
+
+11111, 4444, 4445  
+JBoss  
+https://www.rapid7.com/db/modules/auxiliary/scanner/http/jboss_vulnscan  
+https://github.com/joaomatosf/jexboss  
+  
+4786  
+Cisco Smart Install  
+https://www.rapid7.com/db/modules/auxiliary/scanner/misc/cisco_smart_install  
+https://github.com/Sab0tag3d/SIET  
+  
+5555, 5556  
+HP Data Protector  
+https://www.rapid7.com/db/modules/exploit/multi/misc/hp_data_protector_exec_integutil  
+https://www.rapid7.com/db/modules/exploit/windows/misc/hp_dataprotector_cmd_exec  
+
+3300  
+SAP  
+https://github.com/chipik/SAP_GW_RCE_exploit  
+
+6129  
+Dameware  
+https://www.tenable.com/security/research/tra-2019-43  
+https://github.com/tenable/poc/blob/master/Solarwinds/Dameware/dwrcs_dwDrvInst_rce.py  
+  
+6379  
+Redis  
+https://www.rapid7.com/db/modules/exploit/linux/redis/redis_replication_cmd_exec  
+  
+6970  
+Cisco Unified Commuications Manager  
+http://<ip address>:6970/ConfigFileCacheList.txt  
+  
+8080  
+Adobe CodFusion BlazeDS  
+https://www.tenable.com/plugins/nessus/99731  
+
---
+++ spoonmap.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+
+# Author: Spoonman (Larry.Spohn@TrustedSec.com)
+# QA and Personal Pythonian Consultant: Bandrel (Justin.Bollinger@TrustedSec.com)
+
+import json
+import os
+import subprocess
+import xml.etree.ElementTree as etree
+
+
+def verify_python_version():
+    import sys
+    if sys.version_info[0] == 2:
+        print('Python 3.6+ is required')
+        quit(1)
+    elif sys.version_info[0] == 3 and sys.version_info[1] < 6:
+        print('Python 3.6+ is required')
+        quit(1)
+
+
+def ascii_art():
+    print(r'''
+________                   _____   _______  _________________ 
+__  ___/______________________  | / /__   |/  /__    |__  __ \
+_____ \___  __ \  __ \  __ \_   |/ /__  /|_/ /__  /| |_  /_/ /
+____/ /__  /_/ / /_/ / /_/ /  /|  / _  /  / / _  ___ |  ____/ 
+/____/ _  .___/\____/\____//_/ |_/  /_/  /_/  /_/  |_/_/      
+       /_/                                                 
+    ''')
+
+def mass_scan(scan_type, dest_ports, source_port, max_rate, target_file):
+    status_summary = '\nSummary'
+
+    if not os.path.exists('masscan_results'):
+        os.makedirs('masscan_results')
+    for dest_port in dest_ports:
+
+        # Commence masscan!
+        print('\x1b[33m' + f'Scanning port {dest_port}...' + '\x1b[0m')
+        masscan_process = subprocess.Popen(f'masscan -p {dest_port} --open --max-rate {max_rate} ' \
+            f'--source-port {source_port} -iL {target_file} -oX {dir_path}/masscan_results/port{dest_port}.xml',
+            shell=True)
+        try:
+           masscan_process.wait()
+        except KeyboardInterrupt:
+            print(f'Killing PID {str(masscan_process.pid)}...')
+        if masscan_process.returncode == 1:
+            quit(1)
+
+        # Parse results from masscan
+        if os.stat(f'{dir_path}/masscan_results/port{dest_port}.xml').st_size == 0:
+            os.remove(f'masscan_results/port{dest_port}.xml')
+            print('\x1b[33m' + f'\nHosts Found on Port {dest_port}: 0')
+            print('Masscan Completion Status: ' + '{:.0%}'.format((dest_ports.index(dest_port) + 1) / len(dest_ports)) + '\x1b[0m')
+        else:
+            root = etree.parse(f'{dir_path}/masscan_results/port{dest_port}.xml')
+            hosts = root.findall('host')
+            for host in hosts:
+                ip_address = host.findall('address')[0].attrib['addr']
+                live_port = host.findall('ports/port')[0].attrib['portid']
+
+                # Write live hosts out to file
+                if not os.path.exists(f'{dir_path}/live_hosts'):
+                    os.makedirs(f'{dir_path}/live_hosts')
+                if os.path.exists(f'{dir_path}/live_hosts/port{live_port}.txt'):
+                    with open(f'{dir_path}/live_hosts/port{live_port}.txt') as file:
+                        ip_exists = False
+                        if f'{ip_address}\n' in file.read():
+                            ip_exists = True
+                    if not ip_exists:
+                        with open(f'{dir_path}/live_hosts/port{live_port}.txt', 'a') as file:
+                            file.write(f'{ip_address}\n')
+                else:
+                    with open(f'{dir_path}/live_hosts/port{live_port}.txt', 'w') as file:
+                        file.write(f'{ip_address}\n')
+            host_count = lineCount(f'{dir_path}/live_hosts/port{live_port}.txt')
+            status_update = f'\nHosts Found on Port {dest_port}: {host_count}'
+            status_summary += status_update
+            print('\x1b[33m' + status_update)
+            print('Masscan Completion Status: ' + '{:.0%}'.format((dest_ports.index(dest_port) + 1) / len(dest_ports)) + '\x1b[0m')
+
+    return status_summary
+
+def nmap_scan(source_port):
+
+    # Commence NMAP banner grabbing!
+    if not os.path.exists(f'{dir_path}/nmap_results'):
+        os.makedirs(f'{dir_path}/nmap_results')
+    try:
+        host_files = os.listdir('live_hosts')
+        for host_file in host_files:
+            dest_port = ((host_file.split('.')[0])[4:])
+            if not os.path.exists(f'{dir_path}/nmap_results/port{dest_port}.xml'):
+                print('\x1b[33m' + f'Grabbing service banners for port {dest_port}...\n' + '\x1b[0m')
+                nmap_process = subprocess.Popen(f'nmap -T4 -sS -sV --version-intensity 0 -Pn -p {dest_port} --open ' \
+                    f'--randomize-hosts --source-port {source_port} -iL {dir_path}/live_hosts/port{dest_port}.txt ' \
+                    f'-oX {dir_path}/nmap_results/port{dest_port}.xml',
+                    shell=True)
+                try:
+                    nmap_process.wait()
+                    print('\x1b[33m' + '\nNMAP Completion Status: ' + \
+                        '{:.0%}'.format((host_files.index(host_file) + 1) / len(host_files)) + \
+                        '\x1b[0m')
+                except KeyboardInterrupt:
+                    print(f'Killing PID {str(nmap_process.pid)}...')
+    except:
+        pass
+
+# Counts the number of lines in a file
+def lineCount(file):
+    try:
+        with open(file) as outFile:
+            count = 0
+            for line in outFile:
+                count = count + 1
+        return count
+    except:
+        return 0
+
+
+# The Main Guts
+def main():
+    global dir_path
+    ascii_art()
+
+    scan_type = ''
+    dest_ports = []
+    banner_scan = ''
+    target_scan = ''
+    source_port = '53'
+    max_rate = ''
+    target_file = ''
+    status_summary = ''
+
+
+    # Get options from configuration file if it exists
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    if os.path.exists(f'{dir_path}/config.json'):
+        with open(f'{dir_path}/config.json') as config:
+            config_parser = json.load(config)
+
+        scan_type = config_parser['scan_type']
+        dest_ports = config_parser['dest_ports']
+        banner_scan = config_parser['banner_scan']
+        if banner_scan == 'True':
+            banner_scan = True
+        else:
+            banner_scan = False
+        target_scan = config_parser['target_scan']
+        max_rate = config_parser['max_rate']
+        target_file = config_parser['target_file']
+
+    if scan_type == '':
+        scan_choice = '1'
+        while True:
+            print('\nScan Type')
+            print('\t(1) Small Port Scan')
+            print('\t(2) Medium Port Scan')
+            print('\t(3) Large Port Scan')
+            print('\t(4) Extra Large Port Scan (Small, Medium, and Large)')
+            print('\t(5) Full Port Scan')
+            print('\t(6) Custom Port Scan')
+            scan_choice = input(
+                f'\nWhat type of scan would you like to perform (default: Small Port Scan)? '
+                ) or scan_choice
+            if scan_choice == '1':
+                scan_type = 'Small Port Scan'
+                break
+            elif scan_choice == '2':
+                scan_type = 'Medium Port Scan'
+                break
+            elif scan_choice == '3':
+                scan_type = 'Large Port Scan'
+                break
+            elif scan_choice == '4':
+                scan_type = 'Extra Large Port Scan'
+                break
+            elif scan_choice == '5':
+                scan_type = 'Full Port Scan'
+                break
+            elif scan_choice == '6':
+                scan_type = 'Custom Port Scan'
+                break
+
+    small_ports = ['80', '443', '8000', '8080', '8008', '8181', '8443']
+    medium_ports = ['7001', '1433', '445', '139', '21', '22', '23', '25', 
+                '53', '111', '389', '4243', '3389', '3306', '4786', 
+                '5900', '5901', '6379', '6970', '9100']
+    large_ports = ['1090', '1098', '1099', '10999', '11099', '11111', 
+                '3300', '4243', '4444', '4445', '45000', '45001', 
+                '47001', '47002', '4786', '4848', '50500', '5555', 
+                '5556', '6129', '6379', '6970', '7000', 
+                '7002', '7003', '7004', '7070', '7071', 
+                '8001', '8002', '8003', '8686', '9000', 
+                '9001', '9002', '9003', '9012', '9503']
+    if scan_type == 'Small Port Scan':
+        dest_ports = small_ports
+    elif scan_type == 'Medium Port Scan':
+        dest_ports = medium_ports
+    elif scan_type == 'Large Port Scan':
+        dest_ports = large_ports
+    elif scan_type == 'Extra Large Port Scan':
+        dest_ports = small_ports + medium_ports + large_ports
+    elif scan_type == 'Full Port Scan':
+        dest_ports = ['1-65535']
+    elif scan_type == 'Custom Port Scan' and not dest_ports:
+        dest_ports = input(
+            '\nWhat ports would you like to scan (separated by space: 80 443)? ').split()
+
+    if banner_scan == '':
+        banner_choice = 1
+        banner_choice = input(
+            f'\nWould you like to enumerate service banners for any identified services '
+            f'(default: Yes)? '
+            ) or banner_choice
+        if banner_choice == 1 or banner_choice[0].lower() == 'y':
+            banner_scan = True
+        else:
+            banner_scan = False
+
+    if not target_scan:
+        source_choice = '1'
+        while True:
+            print('\nTarget Scan')
+            print('\t(1) External')
+            print('\t(2) Internal')
+            source_choice = input(
+                f'\nIs this an internal or external scan '
+                f'(default: External)? '
+                ) or source_choice
+            if source_choice == '1':
+                target_scan = 'External'
+                source_port = '53'
+                break
+            elif source_choice == '2':
+                target_scan = 'Internal'
+                source_port = '88'
+                break
+
+    if not max_rate:
+        if target_scan == "External" and scan_type == "Small Port Scan":
+            max_rate = '20000'
+        elif target_scan == "External" and scan_type == "Full Port Scan":
+            max_rate = '10000'
+        elif target_scan == "Internal" and scan_type == "Small Port Scan":
+            max_rate = '2000'
+        elif target_scan == "Internal" and scan_type == "Full Port Scan":
+            max_rate = '1000'
+        else:
+            max_rate = '2000'
+        while True:
+            try:
+                rate_choice = input(f'\nHow fast would you like to scan '
+                    f'(default: {max_rate} packets/second)? '
+                    ) or max_rate
+                if int(rate_choice):
+                    max_rate = rate_choice
+                    break
+            except ValueError:
+                pass
+
+    if not target_file:
+        target_file = 'ranges.txt'
+        while True:
+            print('\nExample Target File')
+            print('One CIDR or IP Address per line\n')
+            print('\t192.168.0.0/24')
+            print('\t192.168.1.23')
+            target_file = input(f'\nPlease enter the full path for the file '
+                f'containing target hosts (default: {dir_path}/{target_file}): '
+                ) or target_file
+            if os.path.exists(target_file):
+                break
+
+    print(f'\nScan Type: {scan_type}')
+    print(f'Target Ports: {dest_ports}')
+    print(f'Service Banner: {banner_scan}')
+    print(f'Source Port: {source_port}')
+    print(f'Masscan Max Packet Rate (pps): {max_rate}')
+    print(f'Target File: {target_file}\n')
+
+    status_summary = mass_scan(scan_type, dest_ports, source_port, max_rate, target_file)
+
+    # If service banners requested, send to nmap
+    if banner_scan or banner_scan == 'Yes':
+        nmap_scan(source_port)
+
+    # Combine all live hosts into one file
+    all_ips = set()
+    if os.path.exists(f'{dir_path}/live_hosts'):
+        host_files = os.listdir(f'{dir_path}/live_hosts')
+        for host_file in host_files:
+            with open(f'{dir_path}/live_hosts/{host_file}') as input_file:
+                for line in input_file:
+                    all_ips.add(line)
+        with open(f'{dir_path}/all_live_hosts.txt', 'w') as output_file:
+            for ip in all_ips:
+                output_file.write(ip)
+
+        # Combine all XML results into one file
+        if banner_scan :
+            result_dir = f'{dir_path}/nmap_results/'
+        else:
+            result_dir = f'{dir_path}/masscan_results/'
+        xml_result = '<?xml version="1.0"?>\n<!-- SpooNMAP -->\n<nmaprun>\n'
+        xml_files = os.listdir(result_dir)
+        for xml_file in xml_files:
+            root = etree.parse(result_dir + xml_file)
+            hosts = root.findall('host')
+            for host in hosts:
+                xml_result += etree.tostring(host, encoding="unicode", method="xml")
+        xml_result += '</nmaprun>'
+        with open(f'{dir_path}/spoonmap_output.xml', 'w+') as spoonmap_output:
+            spoonmap_output.write(xml_result)
+        print('\x1b[33m' + f'\nResults written to {dir_path}/spoonmap_output.xml' + '\x1b[0m')
+
+    else:
+        status_summary += '\nNo hosts found.'
+
+    # Print Summary
+    print('\x1b[33m' + status_summary + '\x1b[0m')
+
+# Boilerplate
+if __name__ == '__main__':
+    verify_python_version()
+    main()
